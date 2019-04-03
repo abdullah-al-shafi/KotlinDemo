522fa317f449cbd4caa7e2997c79c44836885f15
Abdullah Al Shafi <abdullah_al.shafi@outlook.com>
2019-04-03 06:41:43 +0600

    Collections and Strings

---
+++ src/String.kt
@@ -0,0 +1,21 @@
+
+
+fun main(args:Array<String>){
+
+    var titel = " welcome to Bangladesh"
+
+    println(titel)
+
+    println("titel:"+titel)
+    println("titel:$titel")
+
+    var name = "abdullah "+"al "+"shafi"
+
+    println("name: $name")
+    println("Second Char: "+titel[1])
+    println("titel:"+titel.toLowerCase())
+    println("titel:"+titel.toUpperCase())
+    println("titel:"+titel.split(" "))
+    println("titel:"+titel.trim())
+
+}
